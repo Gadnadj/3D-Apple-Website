9db23f938a07da9ad8684d8887bd2077ba0430ea
Gad Nadjar <gadnadjar@gmail.com>
2025-02-25 14:27:34 +0200

    fix carousel

--- src/components/VideoCarousel.tsx
+++ src/components/VideoCarousel.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { hightlightsSlides } from '../constants';
+import { pauseImg, playImg, replayImg } from '../utils';
 
 
 const VideoCarousel = () => {
-    const videoRef = useRef([]);
-    const videoSpanRef = useRef([]);
-    const videoDivRef = useRef([]);
+    const videoRef = useRef<(HTMLVideoElement | null)[]>([]);
+    const videoSpanRef = useRef<(HTMLSpanElement | null)[]>([]);
+    const videoDivRef = useRef<(HTMLSpanElement | null)[]>([]);
     const [video, setVideo] = useState({
         isEnd: false,
         startPlay: false,
@@ -21,10 +22,10 @@ const VideoCarousel = () => {
     useEffect(() => {
         if (loadedData.length > 3) {
             if (!isPlaying) {
-                videoRef.current[videoId].pause();
+                videoRef.current[videoId]?.pause();
             }
             else {
-                startPlay && videoRef.current[videoId].play();
+                startPlay && videoRef.current[videoId]?.play();
             }
         }
     }, [startPlay, videoId, isPlaying, loadedData]);
@@ -50,7 +51,14 @@ const VideoCarousel = () => {
                         <div key={item.id} id='slider' className='sm:pr-20 pr-10'>
                             <div className='video-carousel_container'>
                                 <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
-                                    <video id='video' playsInline={true} preload='auto' muted>
+                                    <video
+                                        id='video'
+                                        playsInline={true}
+                                        preload='auto'
+                                        muted
+                                        ref={(el) => { videoRef.current[i] = el; }}
+                                        onPlay={() => setVideo(prevVideo => ({ ...prevVideo, isPlaying: true }))}
+                                    >
                                         <source src={item.video} type='video/mp4' />
                                     </video>
                                 </div>
@@ -66,6 +74,29 @@ const VideoCarousel = () => {
                         </div>
                     ))
                 }
+            </div >
+
+            <div className='relative flex-center mt-10'>
+                <div className='flex-center py-5 px-7 bg-gray-300 backdrop:blur rounded-full'>
+                    {
+                        videoRef.current.map((_, index) => (
+                            <span
+                                key={index}
+                                ref={(el) => { videoDivRef.current[index] = el; }}
+                                className='mx-2 w-3 h-3 bg-gray-200 rounded-full relarive cursor-pointer'>
+
+                                <span
+                                    className='absolute h-full w-full rounded-full'
+                                    ref={(el) => { videoSpanRef.current[index] = el; }}
+                                />
+                            </span>
+                        ))
+                    }
+                </div>
+
+                <button className='control-btn'>
+                    <img src={isLastVideo ? replayImg : !isPlaying ? playImg : pauseImg} alt={isLastVideo ? 'replay' : !isPlaying ? 'play' : 'pause'} />
+                </button>
             </div>
         </>
     );
